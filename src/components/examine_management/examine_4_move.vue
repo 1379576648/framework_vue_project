8a7a7165466e341858e54489b7fa526beca33240
liyu <dx112729@163.com>
2021-12-17 10:13:27 +0800

    修复按钮没有反馈

--- src/components/examine_management/examine_4_move.vue
+++ src/components/examine_management/examine_4_move.vue
@@ -7,50 +7,71 @@
         <el-button @click="resetDateFilter1">重置日期过滤</el-button>
         &nbsp;
         <el-input
-          v-model="input"
-          placeholder="输入名称搜索"
-          style="width: 130px"
+            v-model="input"
+            placeholder="输入名称搜索"
+            style="width: 130px"
         />
         &nbsp;
         <el-button type="success" plain>搜索</el-button>
         <!--  表格 -->
         <el-table
-          ref="filterTable1"
-          row-key="date1"
-          :data="tableData1"
-          style="width: 100%"
+            ref="filterTable1"
+            row-key="date1"
+            :data="tableData1"
+            style="width: 100%"
         >
           <el-table-column
-            prop="date1"
-            label="日期"
-            sortable
-            width="140"
-            column-key="date1"
-            :filters="[
+              prop="date1"
+              label="日期"
+              sortable
+              width="140"
+              column-key="date1"
+              :filters="[
               { text: '2016-05-01', value: '2016-05-01' },
               { text: '2016-05-02', value: '2016-05-02' },
               { text: '2016-05-03', value: '2016-05-03' },
               { text: '2016-05-04', value: '2016-05-04' },
             ]"
-            :filter-method="filterHandler"
+              :filter-method="filterHandler"
           />
-          <el-table-column prop="AUDITFLOW_ID" label="审批编号" width="100" />
-          <el-table-column prop="AUDITFLOW_TYPE" label="流程" width="100" />
-          <el-table-column prop="STAFF_ID" label="申请人" width="150" />
+          <el-table-column prop="AUDITFLOW_ID" label="审批编号" width="100"/>
+          <el-table-column prop="AUDITFLOW_TYPE" label="流程" width="100"/>
+          <el-table-column prop="STAFF_ID" label="申请人" width="150"/>
           <!-- <el-table-column prop="name" label="操作人" width="100" /> -->
-          <el-table-column prop="AUDITFLOW_STATE" label="状态" width="100" />
-          <el-table-column prop="STAFF_NAME" label="当前审批人" width="150" />
-          <el-table-column prop="UPDATED_TIME" label="最近处理" width="150" />
-          
+          <el-table-column prop="AUDITFLOW_STATE" label="状态" width="100"/>
+          <el-table-column prop="STAFF_NAME" label="当前审批人" width="150"/>
+          <el-table-column prop="UPDATED_TIME" label="最近处理" width="150"/>
+
           <el-table-column label="操作">
             <template #default="scope">
-              <el-button type="success" plain>通过</el-button>
-
-              <el-button type="danger" plain>驳回</el-button>
+              <el-popconfirm
+                  confirm-button-text="确定"
+                  cancel-button-text="取消"
+                  :icon="InfoFilled"
+                  icon-color="red"
+                  title="确定通过吗?"
+                  @confirm="through1()"
+              >
+                <template #reference>
+                  <el-button type="success" plain>通过</el-button>
+                </template>
+              </el-popconfirm>
+              <el-popconfirm
+                  confirm-button-text="确定"
+                  cancel-button-text="取消"
+                  :icon="InfoFilled"
+                  icon-color="red"
+                  title="确定驳回吗?"
+                  @confirm="through2()"
+              >
+                <template #reference>
+                  <el-button type="danger" plain>驳回</el-button>>
+                </template>
+              </el-popconfirm>
               <el-button
-                type="primary"
-                style="margin-left: 16px"
-                @click="drawer = true"
+                  type="primary"
+                  style="margin-left: 16px"
+                  @click="drawer = true"
               >
                 详情
               </el-button>
@@ -60,14 +81,14 @@
         <!-- 分页插件 -->
         <div class="demo-pagination-block">
           <el-pagination
-            v-model:currentPage="pageInfo.currentPage"
-            :page-sizes="[3, 5, 10, 50]"
-            v-model:page-size="pageInfo.pagesize"
-            :default-page-size="pageInfo.pagesize"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="pageInfo.total"
-            :pager-count="5"
-            background
+              v-model:currentPage="pageInfo.currentPage"
+              :page-sizes="[3, 5, 10, 50]"
+              v-model:page-size="pageInfo.pagesize"
+              :default-page-size="pageInfo.pagesize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="pageInfo.total"
+              :pager-count="5"
+              background
           >
             <!--  @size-change="selectUsers" @current-change="selectUsers" -->
           </el-pagination>
@@ -82,46 +103,46 @@
         <el-button @click="resetDateFilter">重置日期过滤</el-button>
         &nbsp;
         <el-input
-          v-model="input"
-          placeholder="输入名称搜索nima"
-          style="width: 130px"
+            v-model="input"
+            placeholder="输入名称搜索nima"
+            style="width: 130px"
         />
         &nbsp;
         <el-button type="success" plain>搜索</el-button>
 
         <el-table
-          ref="filterTable"
-          row-key="date"
-          :data="tableData"
-          style="width: 100%"
+            ref="filterTable"
+            row-key="date"
+            :data="tableData"
+            style="width: 100%"
         >
           <el-table-column
-            prop="date"
-            label="日期"
-            sortable
-            width="140"
-            column-key="date"
-            :filters="[
+              prop="date"
+              label="日期"
+              sortable
+              width="140"
+              column-key="date"
+              :filters="[
               { text: '2016-05-01', value: '2016-05-01' },
               { text: '2016-05-02', value: '2016-05-02' },
               { text: '2016-05-03', value: '2016-05-03' },
               { text: '2016-05-04', value: '2016-05-04' },
             ]"
-            :filter-method="filterHandler"
+              :filter-method="filterHandler"
           />
-          <el-table-column prop="AUDITFLOW_ID" label="审批编号" width="100" />
-          <el-table-column prop="AUDITFLOW_TYPE" label="流程" width="100" />
-          <el-table-column prop="STAFF_ID" label="申请人" width="150" />
+          <el-table-column prop="AUDITFLOW_ID" label="审批编号" width="100"/>
+          <el-table-column prop="AUDITFLOW_TYPE" label="流程" width="100"/>
+          <el-table-column prop="STAFF_ID" label="申请人" width="150"/>
           <!-- <el-table-column prop="name" label="操作人" width="100" /> -->
-          <el-table-column prop="AUDITFLOW_STATE" label="状态" width="100" />
-          <el-table-column prop="STAFF_NAME" label="历史审批人" width="150" />
-          <el-table-column prop="UPDATED_TIME" label="最近处理" width="140" />
+          <el-table-column prop="AUDITFLOW_STATE" label="状态" width="100"/>
+          <el-table-column prop="STAFF_NAME" label="历史审批人" width="150"/>
+          <el-table-column prop="UPDATED_TIME" label="最近处理" width="140"/>
           <el-table-column label="操作">
             <template #default="scope">
               <el-button
-                type="primary"
-                style="margin-left: 5px"
-                @click="drawer = true"
+                  type="primary"
+                  style="margin-left: 5px"
+                  @click="drawer = true"
               >
                 详情
               </el-button>
@@ -131,14 +152,14 @@
         <!-- 分页插件 -->
         <div class="demo-pagination-block">
           <el-pagination
-            v-model:currentPage="pageInfo.currentPage"
-            :page-sizes="[3, 5, 10, 50]"
-            v-model:page-size="pageInfo.pagesize"
-            :default-page-size="pageInfo.pagesize"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="pageInfo.total"
-            :pager-count="5"
-            background
+              v-model:currentPage="pageInfo.currentPage"
+              :page-sizes="[3, 5, 10, 50]"
+              v-model:page-size="pageInfo.pagesize"
+              :default-page-size="pageInfo.pagesize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="pageInfo.total"
+              :pager-count="5"
+              background
           >
             <!--  @size-change="selectUsers"
 						@current-change="selectUsers" -->
@@ -150,15 +171,8 @@
 </template>
 
 <script>
-import {
-  Search,
-  Edit,
-  Check,
-  Message,
-  Star,
-  Delete,
-} from "@element-plus/icons";
-import { defineComponent, ref } from "vue";
+import {defineComponent, ref} from "vue";
+
 export default {
   setup() {
     return {
@@ -320,6 +334,14 @@ export default {
       const property = column["property"];
       return row[property] === value;
     },
+    // 点击通过确认按钮触发
+    through1() {
+      alert(1)
+    },
+    // 点击驳回确认按钮触发
+    through2() {
+      alert(1)
+    }
   },
 };
 </script>
