bd0b446848a3d798a7aaa5ff22adb43b06655a40
袁可颖 <3043949730@qq.com>
2022-02-18 19:33:03 +0800

    完善

--- src/components/salary_management/salary_workovertimeplan.vue
+++ src/components/salary_management/salary_workovertimeplan.vue
@@ -79,7 +79,7 @@
         </div>
 
         <!-- 分页插件 -->
-        <div class="demo-pagination-block">
+        <div class="demo-pagination-block" style="margin-left: 25px;margin-top: 20px;">
           <el-pagination
               v-model:currentPage="pageInfo.currentPage"
               :page-sizes="[3, 5, 10, 50]"
@@ -168,12 +168,6 @@ export default {
   padding-left: 10px;
   text-align: center;
   color: black;
-}
-
-/* 分页的样式 */
-/deep/ .demo-pagination-block {
-  float: right;
-  margin: 20px;
 }
 
 /deep/ .cell {
